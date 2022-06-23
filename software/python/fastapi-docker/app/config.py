34a47ddec1964ab43ed7f00ffde9a1925abc530d
cryptomegabyte <akayanigithub@outlook.com>
2022-06-23 12:05:47 +0100

    Ali K: Added config file

---
+++ software/python/fastapi-docker/app/config.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+from pydantic import BaseSettings
+
+log = logging.getLogger("uvicorn")
+
+class Settings(BaseSettings):
+    """
+    Defines a settings class with two atrributes
+    """
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+def get_settings() -> BaseSettings:
+    """
+    Returns the settings
+      Input: None
+      Return: BaseSettings Class
+    """
+    log.info("Loading config settings from the environment.....")
+    return Settings()
