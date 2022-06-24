7d3caa465290f2f2ee04dda33514f435cc3855ec
cryptomegabyte <akayanigithub@outlook.com>
2022-06-24 10:32:40 +0100

    Ali K: Added db url to settings

--- software/python/text_summarisation_microservice/app/config.py
+++ software/python/text_summarisation_microservice/app/config.py
@@ -2,16 +2,20 @@ import logging
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     """
-    Defines a settings class with two atrributes
+    Defines a settings class with three atrributes
+     - Environment which defaults to dev
+     - Testing Boolean which defaults to 0
+     - Postgres db url
     """
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 @lru_cache
 def get_settings() -> BaseSettings:
