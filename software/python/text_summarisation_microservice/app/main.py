3e1b08da2ca08fe4dd0dcb83007a12c83a505d78
cryptomegabyte <akayanigithub@outlook.com>
2022-06-27 08:26:32 +0100

    Ali K: Added tortoise to main.py

--- software/python/text_summarisation_microservice/app/main.py
+++ software/python/text_summarisation_microservice/app/main.py
@@ -1,11 +1,27 @@
 
 #imports
+from os import environ
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 from app.config import Settings, get_settings
 
 # Main app
 app = FastAPI()
 
+register_tortoise(
+    app,
+    db_url=environ.get("DATABASE_URL"),
+    modules={
+        "models": [
+            "app.models.tortoise"
+            ],
+    },
+        generate_schemas=True,
+        add_exception_handlers=True
+)
+
+#Example route
+# [TODO] - Refactor using fastapi router
 @app.get("/ping")
 async def pong(
     settings: Settings = Depends(get_settings)
