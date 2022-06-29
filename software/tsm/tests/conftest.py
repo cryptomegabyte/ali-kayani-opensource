0f5a6f66d715ed25c740e229527987b3bbdfb157
cryptomegabyte <akayanigithub@outlook.com>
2022-06-29 10:27:34 +0100

    Ali K : Updated conftest with test app with db

--- software/tsm/tests/conftest.py
+++ software/tsm/tests/conftest.py
@@ -7,6 +7,7 @@ from app import main
 from app.config import Settings, get_settings
 from app.main import create_application
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override() -> None:
@@ -23,6 +24,26 @@ def test_app() -> None:
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(main.app) as test_client:
+
+        # testing
+        yield test_client
+
+    # tear down
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
 
         # testing
         yield test_client
