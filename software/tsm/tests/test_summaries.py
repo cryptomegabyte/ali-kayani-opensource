21863601a3f0363e5826f4de688d2d055ccecbce
cryptomegabyte <akayanigithub@outlook.com>
2022-06-29 11:06:09 +0100

    Ali K: Added another /summaries/ route test for exception

--- software/tsm/tests/test_summaries.py
+++ software/tsm/tests/test_summaries.py
@@ -4,9 +4,40 @@ import pytest
 
 
 def test_create_summary(test_app_with_db) -> None:
+    """
+    Tests the /summaries/ default route
+    """
+
+    "Given: test_app_with_db"
+
+    # when
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://test.url"})
     )
 
+    # then
     assert response.status_code == 201
     assert response.json()["url"] == "https://test.url"
+
+
+def test_create_summaries_invalid_json(test_app):
+    """
+    Tests the /summaries/ route for an exception
+    """
+
+    "Given: test_app_with_db"
+
+    # when
+    response = test_app.post("/summaries/", data=json.dumps({}))
+
+    # then
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "url"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
