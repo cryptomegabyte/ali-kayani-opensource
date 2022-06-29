7e1193ac0578ef45f0ab9676b3f67c68058c239f
cryptomegabyte <akayanigithub@outlook.com>
2022-06-29 13:02:31 +0100

    Ali K: Added test for incorrect summary/:id

--- software/tsm/tests/test_summaries.py
+++ software/tsm/tests/test_summaries.py
@@ -68,3 +68,18 @@ def test_read_summary(test_app_with_db) -> None:
     assert response_dict["url"] == url["url"]
     assert response_dict["summary"]
     assert response_dict["created_at"]
+
+
+def test_read_summary_incorrect_id(test_app_with_db):
+    """
+    Tests the /summaries/ default route with incorrect id
+    """
+
+    "Given: test_app_with_db"
+
+    # when
+    response = test_app_with_db.get("/summaries/999/")
+
+    # then
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
