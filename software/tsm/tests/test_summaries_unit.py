068ca5df6106cddad1760f712f94edb570c9c3da
cryptomegabyte <akayanigithub@outlook.com>
2022-07-18 11:54:09 +0100

    Ali K: Added mocked create summary test

--- software/tsm/tests/test_summaries_unit.py
+++ software/tsm/tests/test_summaries_unit.py
@@ -5,8 +5,27 @@ import pytest
 from app.api import crud, summaries
 
 
-def test_create_summary(test_app, monkeypatch):
-    pass
+def test_mocked_create_summary(test_app, monkeypatch):
+    """
+    Tests the /summaries/ post default route
+    """
+
+    "Given: test_app_with_db"
+
+    test_request_payload = {"url": "https://foo.bar"}
+    test_response_payload = {"id": 1, "url": "https://foo.bar"}
+
+    async def mock_post(payload):
+        return 1
+
+    monkeypatch.setattr(crud, "post", mock_post)
+
+    # when
+    response = test_app.post("/summaries/", data=json.dumps(test_request_payload))
+
+    # then
+    assert response.status_code == 201
+    assert response.json() == test_response_payload
 
 
 def test_create_summaries_invalid_json(test_app) -> None:
@@ -39,7 +58,7 @@ def test_create_summaries_invalid_json(test_app) -> None:
     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
 
 
-def test_read_summary(test_app, monkeypatch):
+def test_mocked_read_summary(test_app, monkeypatch):
     pass
 
 
