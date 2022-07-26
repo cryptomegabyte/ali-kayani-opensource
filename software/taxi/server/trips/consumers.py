81fdbfb47cbdfa9d57bbc796d17e93501334bd16
cryptomegabyte <akayanigithub@outlook.com>
2022-07-26 14:43:34 +0100

    Ali K: Added additional logic

--- software/taxi/server/trips/consumers.py
+++ software/taxi/server/trips/consumers.py
@@ -1,5 +1,6 @@
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from trips.models import Trip
 from trips.serialisers import NestedTripSerializer, TripSerializer
 
 
@@ -30,6 +31,12 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 await self.channel_layer.group_add(
                     group="drivers", channel=self.channel_name
                 )
+
+            for trip_id in await self._get_trip_ids(user):
+                await self.channel_layer.group_add(
+                    group=trip_id, channel=self.channel_name
+                )
+
             await self.accept()
 
     async def create_trip(self, message):
@@ -40,6 +47,11 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         # Send rider requests to all drivers.
         await self.channel_layer.group_send(
             group="drivers", message={"type": "echo.message", "data": trip_data}
+        )
+
+        # Add rider to trip group.
+        await self.channel_layer.group_add(
+            group=f"{trip.id}", channel=self.channel_name
         )
 
         await self.send_json(
@@ -59,6 +71,13 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 await self.channel_layer.group_discard(
                     group="drivers", channel=self.channel_name
                 )
+
+            # new
+            for trip_id in await self._get_trip_ids(user):
+                await self.channel_layer.group_discard(
+                    group=trip_id, channel=self.channel_name
+                )
+
         await super().disconnect(code)
 
     async def echo_message(self, message):
@@ -70,3 +89,20 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
             await self.create_trip(content)
         elif message_type == "echo.message":
             await self.echo_message(content)
+
+    @database_sync_to_async
+    def _get_trip_ids(self, user):
+        user_groups = user.groups.values_list("name", flat=True)
+        if "driver" in user_groups:
+            trip_ids = (
+                user.trips_as_driver.exclude(status=Trip.COMPLETED)
+                .only("id")
+                .values_list("id", flat=True)
+            )
+        else:
+            trip_ids = (
+                user.trips_as_rider.exclude(status=Trip.COMPLETED)
+                .only("id")
+                .values_list("id", flat=True)
+            )
+        return map(str, trip_ids)
