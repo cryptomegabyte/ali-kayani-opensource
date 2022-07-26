a39fd3467341ab7517f10711e736b7041089b666
cryptomegabyte <akayanigithub@outlook.com>
2022-07-26 10:49:18 +0100

    Ali K: Added websocket logic

--- software/taxi/server/taxi/routing.py
+++ software/taxi/server/taxi/routing.py
@@ -1,8 +1,15 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "websocket": URLRouter(
+            [
+                path("taxi/", TaxiConsumer.as_asgi()),
+            ]
+        ),
     }
 )
