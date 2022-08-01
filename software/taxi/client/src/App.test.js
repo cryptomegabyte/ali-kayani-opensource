602c492a89729102cc28da5963f79aebaf91a257
cryptomegabyte <akayanigithub@outlook.com>
2022-08-01 16:08:04 +0100

    Ali K: Updated App tests

--- software/taxi/client/src/App.test.js
+++ software/taxi/client/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new Adapter()});
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
+it('should render the app', () => {
+  expect(wrapper).toBeTruthy();
+});
+
+it('should have h1 tag', () => {
+  expect(wrapper.find('h1')).toHaveLength(1);
+  expect(wrapper.find('h1').text()).toBe('Taxi')
 });
