3a155a169252a26f8acb8c69a87b6f646288a225
cryptomegabyte <akayanigithub@outlook.com>
2022-08-01 16:46:25 +0100

    Ali K: Added landing page test

---
+++ software/taxi/client/src/components/landing/Landing.test.js
@@ -0,0 +1,27 @@
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Landing from './Landing';
+
+Enzyme.configure({adapter: new Adapter()});
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<Landing />);
+});
+
+it('should render the Landing component', () => {
+    expect(wrapper).toBeTruthy();
+});
+
+it('should contain Link components', () => {
+  console.log(wrapper)
+  expect(wrapper.find('Link')).toHaveLength(2)
+  expect(wrapper.find('Link').at(0).text()).toBe('Sign up')
+  expect(wrapper.find('Link').at(1).text()).toBe('Log in')
+});
+
+it('should contain h1 tag', () => {
+  expect(wrapper.find('h1')).toHaveLength(1)
+  expect(wrapper.find('h1').text()).toBe('Taxi')
+});
