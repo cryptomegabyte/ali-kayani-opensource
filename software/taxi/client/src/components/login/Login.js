be101b1ca2dce6fd96c61cb2b0adacb6480f4153
cryptomegabyte <akayanigithub@outlook.com>
2022-08-05 10:49:56 +0100

    Ali K: Updated login logic

--- software/taxi/client/src/components/login/Login.js
+++ software/taxi/client/src/components/login/Login.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Breadcrumb, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function Login (props) {
-    return (
-      <>
-        <Link to='/'>Home</Link>
-        <h1>Log in</h1>
-        <p>
-          Don't have an account? <Link to='/sign-up'>Sign up!</Link>
-        </p>
-      </>
-    );
-  }
-  
-  export default Login;
+// changed
+function LogIn (props) {
+  return (
+    <>
+      <Breadcrumb>
+        <Breadcrumb.Item href='/#/'>Home</Breadcrumb.Item>
+        <Breadcrumb.Item active>Log in</Breadcrumb.Item>
+      </Breadcrumb>
+      <Card>
+        <Card.Header>Log in</Card.Header>
+        <Card.Body>
+          <Card.Text className='text-center'>
+            Don't have an account? <Link to='/sign-up'>Sign up!</Link>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default LogIn;
