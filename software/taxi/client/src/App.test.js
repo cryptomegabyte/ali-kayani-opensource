d9df3f14e5afb684db408281ee80178e5893e696
cryptomegabyte <akayanigithub@outlook.com>
2022-08-05 10:03:11 +0100

    Ali K: Added additional tests

--- software/taxi/client/src/App.test.js
+++ software/taxi/client/src/App.test.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { Navbar } from 'react-bootstrap';
 import App from './App';
 
 Enzyme.configure({adapter: new Adapter()});
@@ -11,5 +12,37 @@ beforeEach(() => {
 });
 
 it('should render the app', () => {
+  expect(wrapper).toBeTruthy();
+});
+
+it('should contain a Navbar component', () => {
+  expect(wrapper.find('Navbar')).toBeTruthy();
+});
+
+it('that navbar component should have props', () => {
+  expect(wrapper.find('Navbar').props().bg).toBe('light');
+  expect(wrapper.find('Navbar').props().expand).toBe('lg');
+  expect(wrapper.find('Navbar').props().variant).toBe('light');
+});
+
+it('should contain two container component', () => {
+  expect(wrapper.find('Container')).toHaveLength(2);
+});
+
+it('should contain Link components', () => {
+  expect(wrapper.find('LinkContainer')).toHaveLength(1)
+});
+
+it('that navbar component should have props', () => {
+  expect(wrapper.find('Navbar').props().bg).toBe('light');
+});
+
+it('that navbar component should have props', () => {
+  expect(wrapper.find('Navbar').props().bg).toBe('light');
+  expect(wrapper.find('Navbar').props().expand).toBe('lg');
+  expect(wrapper.find('Navbar').props().variant).toBe('light');
+});
+
+it('should contain an outlet component', () => {
   expect(wrapper.find('Outlet')).toBeTruthy();
 });
