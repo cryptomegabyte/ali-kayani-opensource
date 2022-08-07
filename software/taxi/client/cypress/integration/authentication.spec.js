93126004b0632052109c6d6e898a2b113bd47db3
cryptomegabyte <akayanigithub@outlook.com>
2022-08-07 10:06:56 +0100

    Ali K: Added log out test

--- software/taxi/client/cypress/integration/authentication.spec.js
+++ software/taxi/client/cypress/integration/authentication.spec.js
@@ -9,14 +9,25 @@ describe('Authentication', function () {
 
     it('Can sign up.', function () {
       cy.visit('/#/sign-up');
-      cy.get('input#username').type('gary.cole@example.com');
-      cy.get('input#firstName').type('Gary');
-      cy.get('input#lastName').type('Cole');
+      cy.get('input#username').type('a.user@foo.bar.com');
+      cy.get('input#firstName').type('Foo');
+      cy.get('input#lastName').type('Bar');
       cy.get('input#password').type('pAssw0rd', { log: false });
       cy.get('select#group').select('driver');
       cy.get('input#photo').attachFile('images/photo.jpg');
       cy.get('button').contains('Sign up').click();
       cy.hash().should('eq', '#/log-in');
     });
+
+    it('Can log in.', function () {
+      cy.visit('/#/log-in');
+      cy.get('input#username').type('gary.cole@example.com');
+      cy.get('input#password').type('pAssw0rd', { log: false });
+      cy.get('button').contains('Log in').click();
+      cy.hash().should('eq', '#/');
+      cy.get('button').contains('Log out');
+    });
+
+
   });
   
--- software/taxi/client/src/App.test.js
+++ software/taxi/client/src/App.test.js
@@ -1,6 +1,5 @@
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Navbar } from 'react-bootstrap';
 import App from './App';
 
 Enzyme.configure({adapter: new Adapter()});
