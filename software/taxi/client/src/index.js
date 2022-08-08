86d88eb5220d8a55eb548677e2fc54539c4eb801
cryptomegabyte <akayanigithub@outlook.com>
2022-08-08 10:30:11 +0100

    Ali K: Ensured headers are set

--- software/taxi/client/src/index.js
+++ software/taxi/client/src/index.js
@@ -2,9 +2,14 @@ import 'bootswatch/dist/lumen/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+import App from './App';
+import './index.css';
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 ReactDOM.render(
   <React.StrictMode>
