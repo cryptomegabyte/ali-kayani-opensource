680de9a581060b72e3342e7281d4fd931513950a
cryptomegabyte <akayanigithub@outlook.com>
2022-08-08 10:09:49 +0100

    Ali K: Added login functionality

--- software/taxi/client/src/App.js
+++ software/taxi/client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 // Components
 import Landing from './components/landing/Landing';
@@ -10,12 +10,16 @@ import './App.css';
 
 // changed
 function App () {
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const logIn = (username, password) => setLoggedIn(true);
+
   return (
     <Routes>
-    <Route path='/' element={<Layout />}>
+    <Route path='/' element={<Layout isLoggedIn={isLoggedIn}/>}>
       <Route index element={<Landing />} />
       <Route path='sign-up' element={<SignUp />} />
-      <Route path='log-in' element={<LogIn />} />
+      <Route path='log-in' element={<LogIn logIn={logIn} />} />
     </Route>
   </Routes>
   );
