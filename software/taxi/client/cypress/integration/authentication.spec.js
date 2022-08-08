5646c9d6aff63cf6b592ee8cd0fbdc3ddea76e7a
cryptomegabyte <akayanigithub@outlook.com>
2022-08-08 13:19:47 +0100

    Ali K: Added additional auth tests

--- software/taxi/client/cypress/integration/authentication.spec.js
+++ software/taxi/client/cypress/integration/authentication.spec.js
@@ -20,14 +20,90 @@ describe('Authentication', function () {
     });
 
     it('Can log in.', function () {
+      
+      cy.intercept('POST', 'log_in', {
+        statusCode: 200,
+        body: {
+          'access': 'ACCESS_TOKEN',
+          'refresh': 'REFRESH_TOKEN'
+        }
+      }).as('logIn');
+    
       cy.visit('/#/log-in');
       cy.get('input#username').type('gary.cole@example.com');
       cy.get('input#password').type('pAssw0rd', { log: false });
       cy.get('button').contains('Log in').click();
+    
+      cy.wait('@logIn');
+    
       cy.hash().should('eq', '#/');
       cy.get('button').contains('Log out');
     });
 
+    it('Cannot visit the login page when logged in.', function () {
+      const { username, password } = Cypress.env('credentials');
+    
+      // Log in.
+      cy.intercept('POST', 'log_in', {
+        statusCode: 200,
+        body: {
+          'access': 'ACCESS_TOKEN',
+          'refresh': 'REFRESH_TOKEN'
+        }
+      }).as('logIn');
+      cy.visit('/#/log-in')
+      cy.get('input#username').type(username)
+      cy.get('input#password').type(password, { log: false })
+      cy.get('button').contains('Log in').click()
+      cy.hash().should('eq', '#/')
+      cy.get('button').contains('Log out')
+      cy.wait('@logIn')
+    
+      cy.visit('/#/log-in');
+      cy.hash().should('eq', '#/');
+    });
+
+    it('Cannot visit the sign up page when logged in.', function () {
+      const { username, password } = Cypress.env('credentials');
+      cy.intercept('POST', 'log_in', {
+        statusCode: 200,
+        body: {
+          'access': 'ACCESS_TOKEN',
+          'refresh': 'REFRESH_TOKEN'
+        }
+      }).as('logIn');
+      cy.visit('/#/log-in')
+      cy.get('input#username').type(username)
+      cy.get('input#password').type(password, { log: false })
+      cy.get('button').contains('Log in').click()
+      cy.hash().should('eq', '#/')
+      cy.get('button').contains('Log out')
+      cy.wait('@logIn')
+    
+      cy.visit('/#/sign-up');
+      cy.hash().should('eq', '#/');
+    });
+
+    it('Cannot see links when logged in.', function () {
+      const { username, password } = Cypress.env('credentials');
+      cy.intercept('POST', 'log_in', {
+        statusCode: 200,
+        body: {
+          'access': 'ACCESS_TOKEN',
+          'refresh': 'REFRESH_TOKEN'
+        }
+      }).as('logIn');
+      cy.visit('/#/log-in')
+      cy.get('input#username').type(username)
+      cy.get('input#password').type(password, { log: false })
+      cy.get('button').contains('Log in').click()
+      cy.hash().should('eq', '#/')
+      cy.get('button').contains('Log out')
+      cy.wait('@logIn')
+    
+      cy.get('button#signUp').should('not.exist');
+      cy.get('button#logIn').should('not.exist');
+    });
 
   });
   
