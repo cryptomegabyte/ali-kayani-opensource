aa3b02a0d675ccddf92b2f06b8fd038b4cfb4011
cryptomegabyte <akayanigithub@outlook.com>
2022-08-16 11:31:06 +0100

    Ali K: Added loading trip logic

--- software/taxi/client/src/components/driver/Driver.js
+++ software/taxi/client/src/components/driver/Driver.js
@@ -1,31 +1,79 @@
-import React from "react";
-import { Breadcrumb, Card } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Breadcrumb } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 
 import { isDriver } from "../../services/AuthService";
+import { getTrips } from "../../services/TipService";
+import TripCard from "../trip_card/TripCard";
 
 function Driver(props) {
+  // new
+  const [trips, setTrips] = useState([]);
+
+  // new
+  useEffect(() => {
+    const loadTrips = async () => {
+      const { response, isError } = await getTrips();
+      if (isError) {
+        setTrips([]);
+      } else {
+        setTrips(response.data);
+      }
+    };
+    loadTrips();
+  }, []);
+
   if (!isDriver()) {
     return <Navigate to="/" />;
   }
+
+  // new
+  const getCurrentTrips = () => {
+    return trips.filter((trip) => {
+      return trip.driver !== null && trip.status !== "COMPLETED";
+    });
+  };
+
+  // new
+  const getRequestedTrips = () => {
+    return trips.filter((trip) => {
+      return trip.status === "REQUESTED";
+    });
+  };
+
+  // new
+  const getCompletedTrips = () => {
+    return trips.filter((trip) => {
+      return trip.status === "COMPLETED";
+    });
+  };
   return (
     <>
       <Breadcrumb>
         <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
         <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
       </Breadcrumb>
-      <Card className="mb-3">
-        <Card.Header>Current Trip</Card.Header>
-        <Card.Body>No trips.</Card.Body>
-      </Card>
-      <Card className="mb-3">
-        <Card.Header>Requested Trips</Card.Header>
-        <Card.Body>No trips.</Card.Body>
-      </Card>
-      <Card className="mb-3">
-        <Card.Header>Recent Trips</Card.Header>
-        <Card.Body>No trips.</Card.Body>
-      </Card>
+
+      <TripCard
+        title="Current Trip"
+        trips={getCurrentTrips()}
+        group="driver"
+        otherGroup="rider"
+      />
+
+      <TripCard
+        title="Requested Trips"
+        trips={getRequestedTrips()}
+        group="driver"
+        otherGroup="rider"
+      />
+
+      <TripCard
+        title="Recent Trips"
+        trips={getCompletedTrips()}
+        group="driver"
+        otherGroup="rider"
+      />
     </>
   );
 }
