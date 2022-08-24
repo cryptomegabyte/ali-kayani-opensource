0347f2fd3e2af45aa313889ced3c099f4e0535f8
cryptomegabyte <akayanigithub@outlook.com>
2022-08-24 10:28:22 +0100

    Ali K: Created setup.py

---
+++ python/articles/packaging/add_app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='add_app',
+      version='0.1',
+      description='An app that adds!',
+      url='https://fakeurl.com',
+      author='Foo Bar',
+      author_email='foo.bar@foo.com',
+      license='MIT',
+      packages=['add_app'],
+      zip_safe=False)
