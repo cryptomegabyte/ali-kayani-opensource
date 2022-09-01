76af2e8cbf64aa2bb05249816d7da5acf0e2ea6b
cryptomegabyte <akayanigithub@outlook.com>
2022-09-01 11:36:21 +0100

    Ali K: Added types

--- python/articles/decorators/app/tests/test_multiply.py
+++ python/articles/decorators/app/tests/test_multiply.py
@@ -7,7 +7,7 @@ Tests the multiplication function
 from src.multiply import multiply
 
 @pytest.mark.parametrize("x,y,result",[(3,3,9),(8,8,64),(5,2,10)])
-def test_multiply(x,y,result) -> None:
+def test_multiply( x:int, y:int ,result:int ) -> None:
     
     # given
     test_wrapper = None
