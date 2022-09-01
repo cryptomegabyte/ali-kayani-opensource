3882b49c3333b9bb14e176d5e2e45bdf37754525
cryptomegabyte <akayanigithub@outlook.com>
2022-09-01 09:08:28 +0100

    Ali K: doc string

--- python/challenges/src/division.py
+++ python/challenges/src/division.py
@@ -1,4 +1,9 @@
 def division(x: int, y:int) -> tuple:
+    """
+    Divides numbers.
+    Input: int, int
+    Output: tuple(int part of division, result of x / y)
+    """
     return (
         int(x/y),
         x / y
