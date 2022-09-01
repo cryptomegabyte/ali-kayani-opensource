2591724f5d4ecb52b1c2c8a6578141b70279b21f
cryptomegabyte <akayanigithub@outlook.com>
2022-09-01 11:35:04 +0100

    Ali K: Created multiply test

---
+++ python/articles/decorators/app/tests/test_multiply.py
@@ -0,0 +1,19 @@
+import pytest
+
+"""
+Tests the multiplication function
+"""
+
+from src.multiply import multiply
+
+@pytest.mark.parametrize("x,y,result",[(3,3,9),(8,8,64),(5,2,10)])
+def test_multiply(x,y,result) -> None:
+    
+    # given
+    test_wrapper = None
+
+    # when
+    test_wrapper = multiply(x,y)
+
+    # then
+    assert test_wrapper == result
