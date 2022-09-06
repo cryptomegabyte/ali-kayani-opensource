c116e40589c6bd481283574de0aa2a045a1a22c9
cryptomegabyte <akayanigithub@outlook.com>
2022-09-06 14:54:46 +0100

    Ali K: Added loops test

---
+++ python/challenges/tests/test_loops.py
@@ -0,0 +1,20 @@
+from src.loops import loops
+
+"""
+For all non-negative integers i < n, print i**2.
+"""
+
+def test_loops(n: int) -> list:
+
+    """
+    Tests the loops function
+    """
+
+    # given
+    test_wrapper = None
+
+    # when
+    test_wrapper = loops(5)
+
+    # then
+    assert test_wrapper == [0,1,4,9,16]
