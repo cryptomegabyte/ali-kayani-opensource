8d9f2927481bc3f14f627b62e6f1cc14330e116e
cryptomegabyte <akayanigithub@outlook.com>
2022-09-06 15:14:12 +0100

    Ali K: Added parametrize

--- python/challenges/tests/test_loops.py
+++ python/challenges/tests/test_loops.py
@@ -1,3 +1,4 @@
+import pytest
 from src.loops import loops
 
 """
@@ -5,7 +6,8 @@ For all non-negative integers i < n, print i**2.
 1 <= n <= 20
 """
 
-def test_loops(n: int) -> list:
+@pytest.mark.parametrize("x,result",[(5,[0,1,4,9,16])])
+def test_loops(x: int, result: list) -> list:
 
     """
     Tests the loops function
@@ -15,7 +17,7 @@ def test_loops(n: int) -> list:
     test_wrapper = None
 
     # when
-    test_wrapper = loops(5)
+    test_wrapper = loops(x)
 
     # then
-    assert test_wrapper == [0,1,4,9,16]
+    assert test_wrapper == result
