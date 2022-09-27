a36dbd86c0bba3ebf062fc0be4d05811d8eb14c5
cryptomegabyte <akayanigithub@outlook.com>
2022-09-27 10:51:57 +0100

    Ali K: Updated types

--- python/articles/unpacking/app/functions/merge.py
+++ python/articles/unpacking/app/functions/merge.py
@@ -1,11 +1,11 @@
-def merge(data_a: any, data_b: any) -> tuple:
+def merge(data_a: tuple, data_b: tuple) -> tuple:
     """
     Demonstrates the use of packing operator * to merge.
     """
     merged = (*data_a,*data_b)
     return merged
 
-def merge_list(data_a: any, data_b: any) -> tuple:
+def merge_list(data_a: tuple, data_b: tuple) -> list:
     """
     Demonstrates the use of packing operator * to merge list
     """
