a7c30bfc16ddb524740c0a6a4d19555ff2e79add
cryptomegabyte <akayanigithub@outlook.com>
2022-10-06 10:21:50 +0100

    Ali K: Added fib_gen test

---
+++ python/articles/generators/app/tests/test_fib_gen.py
@@ -0,0 +1,25 @@
+from src.fib_gen import fib_gen
+
+# Behaviours
+# 1. Should calculate the fib sequence given a integer
+
+def test_fib_gen() -> None:
+    # given
+    test_wrapper = None
+
+    # when
+    test_wrapper = fib_gen(5)
+    
+    # then
+
+    test_fib_value = next()
+    assert test_fib_value == 0
+    test_fib_value = next()
+    assert test_fib_value == 1
+    test_fib_value = next()
+    assert test_fib_value == 1
+    test_fib_value = next()
+    assert test_fib_value == 2
+    test_fib_value = next()
+    assert test_fib_value == 3
+
