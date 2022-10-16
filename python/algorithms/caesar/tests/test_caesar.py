019c5b86235c36fb92fee8b70186415256c4ea4c
cryptomegabyte <akayanigithub@outlook.com>
2022-10-16 10:53:54 +0100

    Ali K: Added caesar test

---
+++ python/algorithms/caesar/tests/test_caesar.py
@@ -0,0 +1,25 @@
+from src.caesar import caesar
+import pytest
+
+"""
+Tests for caesar function
+
+Behavious:
+- Should receive a string
+- Encode the string
+- Return the encoded string
+
+"""
+
+@pytest.mark.parametrize("message,encoded_string",[(".","."),("hello","khoor"),("Hello World!","Hhoor Wruog!")])
+def test_caesar(message: str, encoded_string: str) -> None:
+    # given
+    test_wrapper = None
+
+    # when
+    test_wrapper = caesar(message)
+
+    print(test_wrapper)
+
+    # then
+    assert test_wrapper == encoded_string
