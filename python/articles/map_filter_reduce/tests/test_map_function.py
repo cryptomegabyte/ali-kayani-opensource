248f905fe6659810dd1bc773cb8092b15018b2d1
cryptomegabyte <akayanigithub@outlook.com>
2022-10-17 14:42:12 +0100

    Ali K: Extended test

--- python/articles/map_filter_reduce/tests/test_map_function.py
+++ python/articles/map_filter_reduce/tests/test_map_function.py
@@ -1,5 +1,8 @@
 import pytest
-from src.map_function import map_function_a
+from src.map_function import (
+    map_function_a,
+    map_function_b
+)
 """
 Tests for the map function
 
@@ -8,7 +11,7 @@ Behaviours:
 1. Should produce a list of squared numbers
 """
 
-def test_map_function() -> None:
+def test_map_functions() -> None:
     # given
     test_wrapper = None
 
@@ -17,3 +20,9 @@ def test_map_function() -> None:
 
     # then
     assert test_wrapper == [1, 4, 27, 256, 3125]
+
+    # when
+    test_wrapper = map_function_b(["1","2","3","4","5"])
+
+    # then
+    assert test_wrapper == [1,2,3,4,5]
