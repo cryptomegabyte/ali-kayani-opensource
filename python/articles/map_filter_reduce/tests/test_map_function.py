166c418269429cb07108256bf6613c8d3b3b93da
cryptomegabyte <akayanigithub@outlook.com>
2022-10-17 15:16:36 +0100

    Ali K: Added d test

--- python/articles/map_filter_reduce/tests/test_map_function.py
+++ python/articles/map_filter_reduce/tests/test_map_function.py
@@ -1,7 +1,8 @@
 from src.map_function import (
     map_function_a,
     map_function_b,
-    map_function_c
+    map_function_c,
+    map_function_d
 )
 """
 Tests for the map function
@@ -11,6 +12,7 @@ Behaviours:
 1. Should produce a list of squared numbers
 2. Should convert a list of string numbers into ints
 3. Should convert a list of negative numbers into positive ones
+4. Should convert a list of ints tp floats
 """
 
 def test_map_functions() -> None:
@@ -34,4 +36,11 @@ def test_map_functions() -> None:
 
     # then
     assert test_wrapper == [10,9,8,7,6,5]
+
+    # when
+    test_wrapper = map_function_d([1,2,3,4,5])
+
+    # then
+    assert test_wrapper == [1.0,2.0,3.0,4.0,5.0]
+
 
