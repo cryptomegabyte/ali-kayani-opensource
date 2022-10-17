c6c6c9f744c28990518d6f05c25594cbc4ce3b95
cryptomegabyte <akayanigithub@outlook.com>
2022-10-17 15:19:26 +0100

    Ali K: Added map d example code

--- python/articles/map_filter_reduce/src/map_function.py
+++ python/articles/map_filter_reduce/src/map_function.py
@@ -3,7 +3,7 @@ map(function, iterable[, iterable1, iterable2,..., iterableN])
 
 """
 
-def map_function_a(numbers: list) -> list[int]:
+def map_function_a(numbers: list[int]) -> list[int]:
     """
     map function: raise to the power of each number
     input:
@@ -13,7 +13,7 @@ def map_function_a(numbers: list) -> list[int]:
     """
     return list(map(lambda x: x**x, numbers))
 
-def map_function_b(string_numbers: list) -> list[int]:
+def map_function_b(string_numbers: list[str]) -> list[int]:
     """
     map function: converts strings to ints
     input:
@@ -24,13 +24,24 @@ def map_function_b(string_numbers: list) -> list[int]:
 
     return list(map(int, string_numbers))
 
-def map_function_c(string_numbers: list) -> list[int]:
+def map_function_c(string_numbers: list[int]) -> list[int]:
+    """
+    map function: converts negative ints to abs ints
+    input:
+      numbers: list: list of numbers
+    return:
+      list: absolute numbers
+    """
+
+    return list(map(abs, string_numbers))
+
+def map_function_d(string_numbers: list[int]) -> list[float]:
     """
     map function: converts strings to ints
     input:
       numbers: list: list of numbers
     return:
-      list: absoolute numbers
+      list: absolute numbers
     """
 
-    return list(map(abs, string_numbers))
+    return list(map(float, string_numbers))
