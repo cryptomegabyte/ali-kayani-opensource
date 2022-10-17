b8bb838f669045e4a24303f8b6f5188e77e3fa81
cryptomegabyte <akayanigithub@outlook.com>
2022-10-17 14:38:34 +0100

    Ali K: Added doc_string

--- python/articles/map_filter_reduce/src/map_function.py
+++ python/articles/map_filter_reduce/src/map_function.py
@@ -1,2 +1,14 @@
+"""
+map(function, iterable[, iterable1, iterable2,..., iterableN])
+
+"""
+
 def map_function(numbers: list) -> list[int]:
+    """
+    map function: raise to the power of each number
+    input:
+      numbers: list: list of numbers
+    return:
+      list: list of powered numbers
+    """
     return list(map(lambda x: x**x, numbers))
