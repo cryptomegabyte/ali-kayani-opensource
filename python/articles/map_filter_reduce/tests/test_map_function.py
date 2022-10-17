8462db1a84220cd26274557a72f630163e2662a1
cryptomegabyte <akayanigithub@outlook.com>
2022-10-17 14:30:17 +0100

    Ali K: Added map test

---
+++ python/articles/map_filter_reduce/tests/test_map_function.py
@@ -0,0 +1,19 @@
+import pytest
+
+"""
+Tests for the map function
+
+Behaviours:
+
+1. Should produce a list of squared numbers
+"""
+
+def test_map_function() -> None:
+    # given
+    test_wrapper = None
+
+    # when
+    test_wrapper = map_function([1,2,3,4,5])
+
+    # then
+    assert test_wrapper == [1,2,9,16,25]
