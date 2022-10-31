33d319b6cbc7955ba1fae734f5942ead39daf064
cryptomegabyte <akayanigithub@outlook.com>
2022-10-31 15:33:02 +0000

    Ali K: Added set functions

--- python/articles/set/src/set.py
+++ python/articles/set/src/set.py
@@ -4,7 +4,31 @@ from typing import Iterable
 def set_function(x: Iterable) -> set:
     """
     Creates a set and returns it
-    Input: list
+    Input: iterable
     Output: set
     """
     return set(x)
+
+def set_function_union(x: Iterable, y: Iterable) -> set:
+    """
+    Creates a set and returns it
+    Input: iterable
+    Output: set of all elements
+    """
+    return x.union(y) # can also be done like this: x | y
+
+def set_function_intersection(x: Iterable, y: Iterable) -> set:
+    """
+    Creates a set and returns it
+    Input: iterable
+    Output: set - elements common to both sets
+    """
+    return x.intersection(y) # can also be done like this: x & y
+
+def set_function_difference(x: Iterable, y: Iterable) -> set:
+    """
+    Creates a set and returns it
+    Input: iterable
+    Output: set - elements in x but not in y
+    """
+    return x.difference(y) # can also be done like this: x - y
