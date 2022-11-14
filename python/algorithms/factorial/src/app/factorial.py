ccf0fa3093d8f71331858651b1aae7edacd37122
cryptomegabyte <akayanigithub@outlook.com>
2022-11-14 21:11:07 +0000

    Ali K: Added docstring

--- python/algorithms/factorial/src/app/factorial.py
+++ python/algorithms/factorial/src/app/factorial.py
@@ -1,9 +1,13 @@
 from functools import reduce
 
 def factorial(x: int) -> int:
+    """
+    Calculates factorial given a number
+    input x: int : number
+    output int
+    """
     if x == 0:
         return 1
     else:
         numbers = [(i+1) for i in range(x)]
-        return reduce(lambda x,y: x*y, numbers)
-
+    return reduce(lambda x,y: x*y, numbers)
