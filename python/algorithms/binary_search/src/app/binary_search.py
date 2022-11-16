5f86d2d2cd0023aff983d59f0241e503117d48b0
cryptomegabyte <akayanigithub@outlook.com>
2022-11-16 09:53:27 +0000

    Ali K: Updated code for other use cases

--- python/algorithms/binary_search/src/app/binary_search.py
+++ python/algorithms/binary_search/src/app/binary_search.py
@@ -1,11 +1,15 @@
 from math import floor
 
 def binary_search(numbers: list[int], search_item: int) -> int:
-    
-    index = floor(len(numbers)/2)
-    print(f"index: {index}")
-    print(f"search_item: {search_item}")
 
-    if numbers[index] == search_item:
+    index = floor(len(numbers)/2)
+
+    if search_item == numbers[index]:
+        print(f"{type(index)}")
         return index
-    
+        
+    if search_item < numbers[index]:
+        binary_search(numbers[:index], search_item)
+
+    if search_item > numbers[index]:
+        binary_search(numbers[index:], search_item)
