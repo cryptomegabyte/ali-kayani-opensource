77daff6849a37b89915bc1d50febe704fae88398
cryptomegabyte <akayanigithub@outlook.com>
2022-11-16 10:08:19 +0000

    Ali K: Added test

--- python/algorithms/binary_search/src/test/test_binary_search.py
+++ python/algorithms/binary_search/src/test/test_binary_search.py
@@ -7,9 +7,9 @@ Tests for binary search function
 Behaviours:
 
 1. Should return index of search item.
-2. Should return a message if element not found.
+2. Should return a -1 if not found
 """
-@pytest.mark.parametrize("x,y,r",[([4],4,0),([1,2,3,4,5],3,2)])
+@pytest.mark.parametrize("x,y,r",[([4],4,0),([1,2,3,4,5],3,2),([1,2,3,4,5],6,-1)])
 def test_binary_search(x: list[int], y: int, r: int) -> None:
     # given
     test_wrapper = None
